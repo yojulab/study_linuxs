cb7cbf5778d3df42c695729d9b5a2efbf344f0f4
Sanghun Oh <otter.oh@gmail.com>
2025-07-15 12:23:09 +0900

    두번째 올리기

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # study_linuxs
+## powershell (GUI & CLI)
+연습 글을 올립니다.
+- 첫째번째
+- 두번째
+
+[안내글](codes/10_powershells.sh)
---
+++ codes/10_powershells.sh
@@ -0,0 +1,19 @@
+PS C:\Users\Administrator> cd C:\
+PS C:\> cd C:\Develops
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-14   오후 5:05                VMs
